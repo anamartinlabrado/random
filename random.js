cf1ea3bf832af1a2844aea2334ba29c7cbef421b
anamartinlabrado <anamartinlabrado@hotmail.es>
2015-04-24 16:16:05 +0200

    Create random.js

---
+++ random.js
@@ -0,0 +1,8 @@
+# random
+//Math.random() devuelve numero aleatorio entre 0 y 1
+var numero= Math.random();
+var STR = 'MAYOR que 0.5;
+if(numero <= 0.5){
+  str = 'MENOR que 0.5';
+  }
+  console.log('\n' + numero +str + '\n');
